7081bc9c184d28dcb5151b7e4a6d69813d0cf824
Karina D <karinad@Karinas-MacBook-Pro.local>
2018-11-08 04:45:49 -0500

    added authentication and modified styling

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,23 +1,49 @@
-(function() {
-  'use strict';
+window.addEventListener('load', function() {
 
-  // window.onload = function() {
-  //   let message = localStorage.getItem("message") || 'Your message will display here';
-  //   $('#message').html(message);
-  //   $('#display').html(message);
-  // }
-
-  $('#button').click(() => {
-    console.log('click')
-    let message = $('#message').val();
-    console.log(message);
-    $('#display').html(message);
-    // localStorage.setItem("message", message);
+  var webAuth = new auth0.WebAuth({
+    domain: 'pwa-auth.auth0.com',
+    clientID: 'jn1MfRpp07p0v1sqMpb6Ok4y7uCNsiR6',
+    responseType: 'token id_token',
+    scope: 'openid',
+    redirectUri: window.location.href
   });
 
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./service-worker.js')
-             .then(function() { console.log('Service Worker Registered'); });
-  }
-})();
+  var loginBtn = document.getElementById('button');
+
+  loginBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    webAuth.authorize();
+    console.log('click')
+      let message = $('#message').val();
+      console.log(message);
+  });
+
+  (function() {
+    'use strict';
+  
+    // window.onload = function() {
+    //   let message = localStorage.getItem("message") || 'Your message will display here';
+    //   $('#message').html(message);
+    //   $('#display').html(message);
+    // }
+  
+    // $('#button').click(() => {
+    //   console.log('click')
+    //   let message = $('#message').val();
+    //   console.log(message);
+    //   // $('#display').html(message);
+    //   // localStorage.setItem("message", message);
+    // });
+  
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+               .register('./service-worker.js')
+               .then(function() { console.log('Service Worker Registered'); });
+    }
+  });
+
+});
+
+
+
+
